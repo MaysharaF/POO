6b1a4ee068b2f43b2b0ceddf445020486a65bcdd
MaysharaF <mayshara@alunos.utfpr.edu.br>
2019-11-27 22:19:29 -0300

    PontoXY

--- src/projetoparte1/PontoXY.java
+++ src/projetoparte1/PontoXY.java
@@ -15,18 +15,22 @@ public class PontoXY extends Ponto2D {
         this.z = 0;
     }
 
+    @Override
     public double getX() {
         return x;
     }
 
+    @Override
     public void setX(double x) {
         this.x = x;
     }
 
+    @Override
     public double getY() {
         return y;
     }
 
+    @Override
     public void setY(double y) {
         this.y = y;
     }
